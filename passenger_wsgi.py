e9f88c99fd461dcc53364d9dd814751c374ae011
Sabih Usmani <sabihusmani20@gmail.com>
2026-05-24 12:29:43 +0500

    edit requirements.txt and passenegr_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,20 @@
-import sys, os
+import sys
+import os
+import traceback
 
-# Set up the path to your app
-sys.path.append(os.getcwd())
+# Hardwire the path to your new app directory
+app_directory = os.path.dirname(__file__)
+if app_directory not in sys.path:
+    sys.path.insert(0, app_directory)
 
-# Import your Flask instance
-# Note: 'app' is the name of your variable in app.py
-from app import app as application
+try:
+    # Attempt to load your Flask app
+    from app import app as application
+except Exception:
+    # If it crashes, catch the error and print it to the screen
+    error_trace = traceback.format_exc()
+
+
+    def application(environ, start_response):
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        return [b"PYTHON CRASH REPORT:\n\n", error_trace.encode('utf-8')]
